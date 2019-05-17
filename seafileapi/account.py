585171e511c2ff9865164b47f8750af9e672cf9e
Brendan Zerr <bzerr@brainwire.ca>
2019-05-16 23:43:13 -0600

    Restore incorrectly-removed class

--- seafileapi/account.py
+++ seafileapi/account.py
@@ -1,7 +1,35 @@
-from seafileapi.utils import urljoin
+from seafileapi.utils import utf8lize, urljoin
 
 
 class Account(object):
+    """
+    A seafile account
+    """
+    def __init__(self, client, id, email, create_time, is_active, is_staff, usage, total):
+        self.client = client
+        self.id = id
+        self.email = email
+        self.create_time = create_time
+        self.is_active = is_active
+        self.is_staff = is_staff
+        self.usage = usage
+        self.total = total
+
+    @classmethod
+    def from_json(cls, client, account_json):
+        account_json = utf8lize(account_json)
+        account_id = account_json['id']
+        email = account_json['email']
+        create_time = account_json['create_time']
+        is_active = account_json['is_active']
+        is_staff = account_json['is_staff']
+        usage = account_json['usage']
+        total = account_json['total']
+
+        return cls(client, account_id, email, create_time, is_active, is_staff, usage, total)
+
+
+class AccountApi(object):
     def __init__(self, client):
         self.client = client
 
@@ -16,4 +44,3 @@ class Account(object):
             return urljoin(self.client.server, '/client-login/?token={}'.format(token))
         else:
             return None
-
